89e7d51b80b54e124f367d969e944baee630e7a3
HY-ZhengWei <hy.zhengwei@qq.com>
2020-01-06 19:15:14 +0800

    1.0.6
    1. 添加：FutureOperator。equals() 方法的判定

--- src/org/hy/xflow/engine/bean/FutureOperator.java
+++ src/org/hy/xflow/engine/bean/FutureOperator.java
@@ -12,6 +12,7 @@ import org.hy.xflow.engine.common.BaseModel;
  * @author      ZhengWei(HY)
  * @createDate  2019-09-11
  * @version     v1.0
+ *              v2.0  2020-01-16  添加：equals() 方法的判定
  */
 public class FutureOperator extends BaseModel
 {
@@ -156,5 +157,129 @@ public class FutureOperator extends BaseModel
     {
         this.splitProcessID = splitProcessID;
     }
+
     
+
+    /**
+     *
+     * @author      ZhengWei(HY)
+     * @createDate  2020-01-06
+     * @version     v1.0
+     *
+     * @return
+     *
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode()
+    {
+        int v_HashCode = 0;
+        
+        if ( this.workID != null )
+        {
+            v_HashCode += this.workID.hashCode();
+        }
+        
+        if ( this.processID != null )
+        {
+            v_HashCode += this.processID.hashCode();
+        }
+        
+        if ( this.objectType != null )
+        {
+            v_HashCode += this.objectType.hashCode();
+        }
+        
+        if ( this.objectID != null )
+        {
+            v_HashCode += this.objectID.hashCode();
+        }
+        
+        return v_HashCode;
+    }
+
+
+    
+    /**
+     *
+     * @author      ZhengWei(HY)
+     * @createDate  2020-01-06
+     * @version     v1.0
+     *
+     * @param i_Other
+     * @return
+     *
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object i_Other)
+    {
+        if ( i_Other == null )
+        {
+            return false;
+        }
+        else if ( this == i_Other )
+        {
+            return true;
+        }
+        else if ( i_Other instanceof FutureOperator )
+        {
+            FutureOperator v_Other = (FutureOperator)i_Other;
+            
+            if ( this.workID != null )
+            {
+                if ( !this.workID.equals(v_Other.getWorkID()) )
+                {
+                    return false;
+                }
+            }
+            else if ( v_Other.getWorkID() != null )
+            {
+                return false;
+            }
+            
+            if ( this.processID != null )
+            {
+                if ( !this.processID.equals(v_Other.getProcessID()) )
+                {
+                    return false;
+                }
+            }
+            else if ( v_Other.getProcessID() != null )
+            {
+                return false;
+            }
+            
+            if ( this.objectType != null )
+            {
+                if ( !this.objectType.equals(v_Other.getObjectType()) )
+                {
+                    return false;
+                }
+            }
+            else if ( v_Other.getObjectType() != null )
+            {
+                return false;
+            }
+            
+            if ( this.objectID != null )
+            {
+                if ( !this.objectID.equals(v_Other.getObjectID()) )
+                {
+                    return false;
+                }
+            }
+            else if ( v_Other.getObjectID() != null )
+            {
+                return false;
+            }
+            
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
