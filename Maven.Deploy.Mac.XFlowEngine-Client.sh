e9362990e8d625c6124e3c5da3b1edad3f239668
ZhengWei <HY.ZhengWei@qq.com>
2023-02-28 14:51:20 +0800

    1.0.9
    1. 修改：配置文件

--- Maven.Deploy.Mac.XFlowEngine-Client.sh
+++ Maven.Deploy.Mac.XFlowEngine-Client.sh
@@ -3,5 +3,5 @@
 mvn install:install-file -Dfile=hy.xflow.engine-client.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.xflow.engine-client/pom.xml
 mvn install:install-file -Dfile=hy.xflow.engine-client-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.xflow.engine-client/pom.xml
 
-mvn deploy:deploy-file   -Dfile=hy.xflow.engine-client.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.xflow.engine-clientpom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
-mvn deploy:deploy-file   -Dfile=hy.xflow.engine-client-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.xflow.engine-clientpom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+mvn deploy:deploy-file   -Dfile=hy.xflow.engine-client.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.xflow.engine-client/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+mvn deploy:deploy-file   -Dfile=hy.xflow.engine-client-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.xflow.engine-client/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
